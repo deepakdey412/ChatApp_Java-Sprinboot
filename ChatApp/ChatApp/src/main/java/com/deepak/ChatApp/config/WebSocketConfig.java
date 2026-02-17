d4162c5386f38085e571f055f3de9bb276f5b428
deepakdey412@gmail.com <deepakdey412@gmail.com>
2026-02-18 00:27:48 +0530

    update in config

--- ChatApp/ChatApp/src/main/java/com/deepak/ChatApp/config/WebSocketConfig.java
+++ ChatApp/ChatApp/src/main/java/com/deepak/ChatApp/config/WebSocketConfig.java
@@ -12,11 +12,14 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        WebSocketMessageBrokerConfigurer.super.registerStompEndpoints(registry);
+        registry.addEndpoint("/chat")
+                .setAllowedOrigins("http://localhost:5173")
+                .withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        WebSocketMessageBrokerConfigurer.super.configureMessageBroker(registry);
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
     }
 }
