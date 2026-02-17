f077250fe82a1b9b385a4d0fc8a1db378f35b802
deepakdey412@gmail.com <deepakdey412@gmail.com>
2026-02-18 00:21:52 +0530

    Project folder added

---
+++ ChatApp/ChatApp/src/main/java/com/deepak/ChatApp/config/WebSocketConfig.java
@@ -0,0 +1,7 @@
+package com.deepak.ChatApp.config;
+
+
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+}
