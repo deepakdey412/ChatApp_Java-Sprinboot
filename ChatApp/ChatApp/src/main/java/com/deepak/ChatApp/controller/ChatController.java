3d4c8d0636b6ba482c739e18f6dcdc7a8944d7ee
deepakdey412@gmail.com <deepakdey412@gmail.com>
2026-02-18 00:34:33 +0530

    update  model and controller

--- ChatApp/ChatApp/src/main/java/com/deepak/ChatApp/controller/ChatController.java
+++ ChatApp/ChatApp/src/main/java/com/deepak/ChatApp/controller/ChatController.java
@@ -1,11 +1,23 @@
 package com.deepak.ChatApp.controller;
 
+import com.deepak.ChatApp.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class ChatController {
+
+    @MessageMapping("/sendmessage")
+    @SendTo("/topic/messages")
     public ChatMessage sendMessage(ChatMessage message){
         return message;
+    }
+
+    @GetMapping("/chat")
+    public String chat(){
+        return "chat";
     }
 }
